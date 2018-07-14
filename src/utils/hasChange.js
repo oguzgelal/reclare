07424742caf8b984c67bba12b5a1c7747fd3f724
Oguz Gelal <oguz.gelal@vub.ac.be>
2018-07-14 16:42:11 +0300

    cleaner syntax for some utils

--- src/utils/hasChange.js
+++ src/utils/hasChange.js
@@ -1,9 +1,7 @@
 import get from 'lodash/get';
 
 export default ({ state, prevState }) => path => {
-  if (!path || path === '') {
-    return state !== prevState;
-  }
+  if (!path || path === '') return state !== prevState;
   const oldVal = get(prevState, path, null);
   const newVal = get(state, path, null);
   return newVal !== oldVal;
--- src/utils/toArray.js
+++ src/utils/toArray.js
@@ -1,12 +1,6 @@
 export default (arr, options = {}) => {
-  if (Array.isArray(arr)) {
-    return arr;
-  }
-  if (arr === undefined && options.dropUndefined) {
-    return [];
-  }
-  if (arr === null && options.dropNull) {
-    return [];
-  }
+  if (Array.isArray(arr)) return arr;
+  if (arr === undefined && options.dropUndefined) return [];
+  if (arr === null && options.dropNull) return [];
   return [arr];
 };
